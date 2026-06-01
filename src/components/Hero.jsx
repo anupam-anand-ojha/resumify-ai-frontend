2480820e4336f69596d71238473e3c61b91254ca
Anupam Anand <theanupamojha999@gmail.com>
2026-06-01 20:32:37 +0530

    add rotating heo text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,23 +25,23 @@ function Hero() {
   <div className="hero-overlay"></div>
 
   <div className="hero-content text-neutral-content text-center">
-    <div className="max-w-md">
+    <div className="max-w-3xl">
       <h1 className="mb-5 text-2xl font-bold">
        <span>
-   Land More Interviews With
+   Build Your Resume With{" "}
   <span className="text-rotate">
     <span>
       <span className="bg-teal-400 text-teal-800 px-2"> RESUMIFY AI</span>
-      <span className="bg-red-400 text-red-800 px-2">INTELLIGENT RESUME BUILDING</span>
-      <span className="bg-blue-400 text-blue-800 px-2">AI-POWERED WRITING</span>
+      <span className="bg-red-400 text-red-800 px-2">AI PRECISION</span>
+      <span className="bg-blue-400 text-blue-800 px-2">ATS OPTIMIZED</span>
     </span>
   </span>
 </span>
       </h1>
       
       <p className="mb-5">
-        Create professional, ATS-friendly resumes instantly using AI. 
-        Just enter your details and get a clean, job-ready resume in seconds
+      Create professional, ATS optimized resumes in minutes with the power of AI.
+       Simply enter your details and generate a polished, job ready resume designed to stand out to recruiters.
       </p>
       <button onClick={ () => Navigate('/resume')} className="btn btn-primary">Create Resume</button>
     </div>
