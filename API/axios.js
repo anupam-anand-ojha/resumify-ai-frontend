7badf33f94d5b80130aeb934047613cf954b1bcb
Anupam Anand <theanupamojha999@gmail.com>
2026-06-18 23:51:48 +0530

    setup environment based api url

--- API/axios.js
+++ API/axios.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL:"http://localhost3000",
-    withCredentials:true
-})
-
-export default API
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
+});
